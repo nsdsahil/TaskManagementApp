b957a6a52b1f0f2424a1e472c733db8cfd0f79a2
Sahil Nishad <sahilroyal91@gmail.com>
2024-05-19 19:00:59 +0530

    all files uploadedd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         <Route path="/" element={<HomePage />} exact />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/dashboard" element={<DashboardPage/>} />
         </Routes>
       </div>
   );
